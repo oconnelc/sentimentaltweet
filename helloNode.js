d09ccb4a128c38be74e9129084722f434b12d1a7
Christopher O'Connell <coconnell84@gmail.com>
2016-08-07 18:39:06 +0000

    Initial Creation

--- helloNode.js
+++ helloNode.js
@@ -1,10 +1,13 @@
-var Twitter = require('twitter'); 
+var Twitter = require('twitter');
+var sentiment = require('sentiment');
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('properties.txt');
 
-var client = new Twitter( { 
- consumer_key: '1DMg575YNht8Hj3yumddFMyAx',
- consumer_secret: 'omoems2MYsO1GBZ4V7KWrsjJdxqiBmyUHpXr2ZWYhwwvFKif2k',
- access_token_key: '269499555-Z6pUDa0UCcKMNMJiYvppae94MkTyw7PMkKgUVhXc',
- access_token_secret: 'sKtCUU3JriU4RFYjjqalAnfv2kTZVCN86tE8HdQ8HF3eg'
+var client = new Twitter({
+  consumer_key: properties.get('twitter.consumer_key'),
+  consumer_secret: properties.get('twitter.consumer_secret'),
+  access_token_key: properties.get('twitter.access_token_key'),
+  access_token_secret: properties.get('twitter.access_token_secret')
 });
 
 
@@ -12,16 +15,27 @@ var client = new Twitter( {
 var five = require('johnny-five');
 var board = new five.Board();
 
-board.on("ready", function() { 
+board.on("ready", function() {
 
-	var led = new five.Led(13);
-	client.stream('statuses/filter', {track: 'cat'}, function(stream) {
-            stream.on('data', function(tweet) { 
-                console.log(tweet.text);
-                led.blink(); 
-                board.wait(100, function() {
-                   led.off();
-                });
-		});
+  var badLed = new five.Led(12);
+  var goodLed = new five.Led(13);
+  client.stream('statuses/filter', {
+    track: 'Donald Trump'
+  }, function(stream) {
+    stream.on('data', function(tweet) {
+      console.log(tweet.text);
+      var tweetSentiment = sentiment(tweet.text);
+
+      console.dir('sentiment score: ' + tweetSentiment.score);
+
+      if(tweetSentiment.score != 0) {
+        var ledToUse = ((tweetSentiment.score < 0) ? badLed : goodLed);
+        ledToUse.blink();
+        board.wait(100, function() {
+          console.log('TURNING OFF');
+          ledToUse.stop().off();
         });
+      }
+    });
+  });
 });
