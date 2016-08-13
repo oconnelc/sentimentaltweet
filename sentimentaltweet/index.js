7f4b4e0d7416b083cc9fca3d517adbe7dcc22f3b
Christopher O'Connell <coconnell84@gmail.com>
2016-08-13 19:49:02 +0000

    Fixed handlebars layout

--- sentimentaltweet/index.js
+++ sentimentaltweet/index.js
@@ -51,15 +51,42 @@ app.use(function(req, res, next){
 });
 
 // Configure express to use handlebars templates
-var hbs = exphbs.create({
-    defaultLayout: 'main', //we will be creating this layout shortly
-});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('.hbs',exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 //===============ROUTES===============
+//displays our homepage
+app.get('/', function(req, res){
+  res.render('home', {user: req.user});
+});
 
-//This section will hold our Routes
+//displays our signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
 
 //===============PORT=================
 var port = process.env.PORT || 3000; //select your port or let it pull from your .env file
